fdffc1fd6c671521f9c3d2cf7f92a0ae25f11523
SUDIP DAS <sdas@advensoftinc.com>
2017-11-10 12:28:27 +0530

    update Feature1

--- ConsoleApp1/ConsoleApp1/Feature1.cs
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -6,5 +6,12 @@ namespace ConsoleApp1
 {
     class Feature1
     {
+        int add()
+        {
+            int x1=1;
+            int x2=2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
