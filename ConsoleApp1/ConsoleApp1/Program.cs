a9c989d0dfa2409d6dd83bccddb5126915943ceb
SUDIP DAS <sdas@advensoftinc.com>
2017-11-10 11:44:13 +0530

    Edit done by VS

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Again Edit
+            //Edit by VS
             Console.WriteLine("Hello World!");
         }
     }
